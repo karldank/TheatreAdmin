7285ad4f1e207b38905ab91b6af0ee7c1125f875
KarlD <80576177+karldank@users.noreply.github.com>
2021-10-25 13:13:43 +0930

    Add Entities

---
+++ MVCMusicApp/Data/MVCMusicAppContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
+
+namespace MVCMusicApp.Data
+{
+    public class MVCMusicAppContext : DbContext
+    {
+        public MVCMusicAppContext (DbContextOptions<MVCMusicAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcMovie.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ MVCMusicApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MVCMusicApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
